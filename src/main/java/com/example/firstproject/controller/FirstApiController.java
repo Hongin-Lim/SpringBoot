68976c2e37627340f2a22f2b41f6ea0c8c9883a1
Hongin Lim <96184680+Hongin-Lim@users.noreply.github.com>
2022-08-26 14:06:11 +0900

    service1

--- src/main/java/com/example/firstproject/controller/FirstApiController.java
+++ src/main/java/com/example/firstproject/controller/FirstApiController.java
@@ -1,2 +1,15 @@
-package com.example.firstproject.controller;public class FirstApiController {
+package com.example.firstproject.controller;
+
+import com.sun.org.apache.bcel.internal.generic.ARETURN;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // RestAPI용 컨트롤러! JSON을 반환 !
+public class FirstApiController {
+
+    @GetMapping("/api/hello")
+    public String hello(){
+        return "hello world!";
+    }
 }
